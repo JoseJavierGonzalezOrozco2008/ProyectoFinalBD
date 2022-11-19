2ef054916279676e913752b7c0883745f4bd1a8a
Javier González <jg3510244@gmail.com>
2022-11-19 08:30:31 -0600

    Implementación Navegacion en App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import React from 'react';
 import Login from './src/screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Routes from './src/screens/Personal_Screen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return (
+  
     
-    <Login/>
-  )
+    return (
+        <NavigationContainer>  
+            <Stack.Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name='Login' component={Login}/>
+                <Stack.Screen name='Personal' component={Routes}/> 
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+
+
 }
 
 export default App
