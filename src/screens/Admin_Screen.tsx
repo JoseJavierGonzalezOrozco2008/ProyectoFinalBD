fd92447c6c280f01fc25efd9f981ccee4ce4cf6c
Javier González <jg3510244@gmail.com>
2022-11-22 17:45:35 -0600

    Nuevas opciones en pantalla de Administrador

--- src/screens/Admin_Screen.tsx
+++ src/screens/Admin_Screen.tsx
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
-import { NavigationContainer, RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import {
+  NavigationContainer,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import {
   createStackNavigator,
   StackNavigationProp,
-} from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /*
 const AlimentarAnimal = require('./assets/secciones/alimentar.png');
@@ -22,133 +34,174 @@ const Compras = require('./assets/secciones/nuevo.png');
 
 interface RoutesProps {}
 
-
-let res:string = "";
-let resPuesto:string = "";
+let res: string = '';
+let resPuesto: string = '';
 //let obj: MyObj = JSON.parse("{'user': 'Hola'}");
 
-interface MyObj{
-  nombre: string,
+interface MyObj {
+  nombre: string;
 }
 
-interface MyObj1{
-  puesto: string,
+interface MyObj1 {
+  puesto: string;
 }
 const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
-
-
 const Registros = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableOpacity style={styles.button} onPress = {() => navigation.navigate('RegProv' as never)}>
-       <Image source={require('../../assets/secciones/RegProveedor.jpg')} style={styles.image} />
-      </TouchableOpacity>
-      <Text> </Text>
-      <TouchableOpacity style={styles.button2}>
-      <Image source={require('../../assets/secciones/RegCliente.jpg')} style={styles.image} />
-      </TouchableOpacity>
-      <Text> </Text>
-      <TouchableOpacity style={styles.button3}>
-       <Image source={require('../../assets/secciones/RegAlimento.jpg')} style={styles.image} />
-      </TouchableOpacity>
-    </View>
+    <ScrollView style={styles.scroll}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('RegProv' as never)}>
+          <Image
+            source={require('../../assets/secciones/RegProveedor.jpg')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <Text> </Text>
+        <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('RegCli' as never)}>
+          <Image
+            source={require('../../assets/secciones/RegCliente.jpg')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <Text> </Text>
+        <TouchableOpacity style={styles.button3} onPress={() => navigation.navigate('RegAlimAnim' as never)}>
+          <Image
+            source={require('../../assets/secciones/RegAlimento.jpg')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+         <Text> </Text>
+        <TouchableOpacity style={styles.button3} onPress={() => navigation.navigate('RegMatPrim' as never)}>
+          <Image
+            source={require('../../assets/secciones/RegMateriaPrima.jpg')}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        <Text> </Text>
+ 
+      </View>
+    </ScrollView>
   );
 };
 
 const Existencias = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <TouchableOpacity style={styles.button}>
-       <Image source={require('../../assets/secciones/ConsAnimal.jpg')} style={styles.image} />
+        <Image
+          source={require('../../assets/secciones/ConsAnimal.jpg')}
+          style={styles.image}
+        />
       </TouchableOpacity>
-      <Text>  </Text>
+      <Text> </Text>
       <TouchableOpacity style={styles.button2}>
-       <Image source={require('../../assets/secciones/ConsMateria.jpg')} style={styles.image} />
+        <Image
+          source={require('../../assets/secciones/ConsMateria.jpg')}
+          style={styles.image}
+        />
       </TouchableOpacity>
-      <Text>  </Text>
+      <Text> </Text>
       <TouchableOpacity style={styles.button3}>
-       <Image source={require('../../assets/secciones/ConsActivi.jpg')} style={styles.image} />
+        <Image
+          source={require('../../assets/secciones/ConsActivi.jpg')}
+          style={styles.image}
+        />
       </TouchableOpacity>
-      <Text>  </Text>
+      <Text> </Text>
     </View>
   );
 };
 
-
 interface HomeProps {}
-export const Usuario =  ({}: HomeProps) => {
-
+export const Usuario = ({}: HomeProps) => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.userContainer}>
-      <Image source={require('../../assets/Iconos/IconoAdmin.jpg')} style={styles.userImg}/>
+      <Image
+        source={require('../../assets/Iconos/IconoAdmin.jpg')}
+        style={styles.userImg}
+      />
       <Text style={styles.userText}>{res}</Text>
       <Text style={styles.textoPuesto}>{resPuesto}</Text>
-      <TouchableOpacity style={styles.logoutBtn} onPress = {() => navigation.navigate('Login' as never)}>
-       <Image source={require('../../assets/logout.jpg')} style={styles.image} />
+      <TouchableOpacity
+        style={styles.logoutBtn}
+        onPress={() => navigation.navigate('Login' as never)}>
+        <Image
+          source={require('../../assets/logout.jpg')}
+          style={styles.image}
+        />
       </TouchableOpacity>
     </View>
   );
 };
 
 const AppTab = () => {
-
-
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-
-          if (route.name === "Usuario") {
-            return <Image source={require('../../assets/Iconos/userIcon.png')} style={styles.icon}/>;
-          } else if (route.name === "Registros") {
-            return <Image source={require('../../assets/Iconos/IconoRegistro.jpg')} style={styles.icon}/>;
-          }else if(route.name === 'Existencias'){
-            return <Image source={require('../../assets/Iconos/IconoExistencias.jpg')} style={styles.icon}/>;
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          if (route.name === 'Usuario') {
+            return (
+              <Image
+                source={require('../../assets/Iconos/userIcon.png')}
+                style={styles.icon}
+              />
+            );
+          } else if (route.name === 'Registros') {
+            return (
+              <Image
+                source={require('../../assets/Iconos/IconoRegistro.jpg')}
+                style={styles.icon}
+              />
+            );
+          } else if (route.name === 'Existencias') {
+            return (
+              <Image
+                source={require('../../assets/Iconos/IconoExistencias.jpg')}
+                style={styles.icon}
+              />
+            );
           }
         },
-        headerShown:false,
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
+        headerShown: false,
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
       })}>
-      
-      <Tab.Screen name="Usuario" component={Usuario}/>
-      <Tab.Screen name="Registros" component={Registros}/>
+      <Tab.Screen name="Usuario" component={Usuario} />
+      <Tab.Screen name="Registros" component={Registros} />
       <Tab.Screen name="Existencias" component={Existencias} />
     </Tab.Navigator>
   );
 };
 
-export function getNameAdm(nombre: any){
-  let obj:MyObj= JSON.parse(nombre)
-  res = obj.nombre
-  console.log(res,' desde Personal_Screen')
-
+export function getNameAdm(nombre: any) {
+  let obj: MyObj = JSON.parse(nombre);
+  res = obj.nombre;
+  console.log(res, ' desde Personal_Screen');
 }
 
-export function getPuestoAdm(puestoAd:any){
-  console.log("valor al momento de entrar a la funcion ",puestoAd)
-  let ps:MyObj1 = JSON.parse(puestoAd)
-  resPuesto = ps.puesto
-  console.log(resPuesto, ' desde Personal Screen')
+export function getPuestoAdm(puestoAd: any) {
+  console.log('valor al momento de entrar a la funcion ', puestoAd);
+  let ps: MyObj1 = JSON.parse(puestoAd);
+  resPuesto = ps.puesto;
+  console.log(resPuesto, ' desde Personal Screen');
 }
-
-
 
 const Routes1 = ({}: RoutesProps) => {
   return (
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Home" component={AppTab} />
-      </Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={AppTab} />
+    </Stack.Navigator>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -169,17 +222,17 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
-    backgroundColor: "#FFC8C8",
+    backgroundColor: '#FFC8C8',
     padding: 10,
     borderRadius: 5,
   },
   button2: {
-    backgroundColor: "orange",
+    backgroundColor: 'orange',
     padding: 10,
     borderRadius: 5,
   },
   button3: {
-    backgroundColor: "orange",
+    backgroundColor: 'orange',
     padding: 10,
     borderRadius: 5,
   },
@@ -194,9 +247,9 @@ const styles = StyleSheet.create({
   },
   icon: {
     width: 40,
-    height: 35
+    height: 35,
   },
-  userContainer:{
+  userContainer: {
     display: 'flex',
     flexDirection: 'column',
     flexWrap: 'nowrap',
@@ -204,33 +257,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignContent: 'center',
     backgroundColor: 'white',
-    height: '100%'
+    height: '100%',
   },
   userImg: {
     width: 180,
     height: 180,
     borderRadius: 100,
     top: -35,
-    padding: 20
-
+    padding: 20,
   },
-  userText:{
+  userText: {
     fontSize: 45,
     top: -40,
-    color: 'black'
-
+    color: 'black',
   },
   textoPuesto: {
     fontSize: 25,
-    top:-40,
-    color: 'gray'
+    top: -40,
+    color: 'gray',
   },
   logoutBtn: {
     top: 90,
     backgroundColor: 'white',
-
+  },
+  scroll: {
+    paddingTop: 40
   }
-
 });
 
-export default Routes1 
+export default Routes1;
